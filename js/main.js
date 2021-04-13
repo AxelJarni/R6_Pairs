bcfaa281a765fbb6a7c001a0371c544d309153dd
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-04-13 12:01:53 +0200

    added cardArray

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const cardArray = [
+    {
+        name: 'Ela',
+        img: 'img/Square/ElaSquare.png'
+    },
+    {
+        name: 'Ela',
+        img: 'img/Square/ElaSquare.png'
+    },
+    {
+        name: 'Frost',
+        img: 'img/Square/FrostSquare.png'
+    },
+    {
+        name: 'Frost',
+        img: 'img/Square/FrostSquare.png'
+    },
+    {
+        name: 'Maestro',
+        img: 'img/Square/NomadSquare.png'
+    },
+    {
+        name: 'Maestro',
+        img: 'img/Square/NomadSquare.png'
+    },
+    {
+        name: 'Nomad',
+        img: 'img/Square/NomadSquare.png'
+    },
+    {
+        name: 'Nomad',
+        img: 'img/Square/NomadSquare.png'
+    },
+    {
+        name: 'Tachanka',
+        img: 'img/Square/TachankaSquare.png'
+    },
+    {
+        name: 'Tachanka',
+        img: 'img/Square/TachankaSquare.png'
+    },
+    {
+        name: 'Thermite',
+        img: 'img/Square/ThermiteSquare.png'
+    },
+    {
+        name: 'Thermite',
+        img: 'img/Square/ThermiteSquare.png'
+    },
+    {
+        name: 'Valkyrie',
+        img: 'img/Square/ValkyrieSquare.png'
+    },
+    {
+        name: 'Valkyrie',
+        img: 'img/Square/ValkyrieSquare.png'
+    }
+];
